fa7237a74e7059033d0c037ea08a3df7c5216858
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2022-12-05 12:00:21 -0300

    feat: Preenchendo mapa com paradas e onibus

--- app/src/main/java/com/example/rastreioonibus/AppMapFragment.kt
+++ app/src/main/java/com/example/rastreioonibus/AppMapFragment.kt
@@ -6,11 +6,13 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.coroutines.*
 import org.koin.android.ext.android.inject
 
-class AppMapFragment : SupportMapFragment(){
+class AppMapFragment : SupportMapFragment() {
     private val viewModel: MapsViewModel by inject()
 
     private val scope = CoroutineScope(Dispatchers.Main + SupervisorJob())
@@ -20,7 +22,7 @@ class AppMapFragment : SupportMapFragment(){
 
     private var googleMap: GoogleMap? = null
 
-    override fun getMapAsync(callback: OnMapReadyCallback){
+    override fun getMapAsync(callback: OnMapReadyCallback) {
         super.getMapAsync {
             scope.launch {
                 googleMap = it
@@ -30,7 +32,7 @@ class AppMapFragment : SupportMapFragment(){
         }
     }
 
-     private suspend fun initMap(map: GoogleMap?) {
+    private suspend fun initMap(map: GoogleMap?) {
         val origin = LatLng(-23.561706, -46.655981)
 
         scope.launch {
@@ -38,11 +40,34 @@ class AppMapFragment : SupportMapFragment(){
         }.join()
 
         googleMap = map?.apply {
-            mapType = GoogleMap.MAP_TYPE_SATELLITE
+            mapType = GoogleMap.MAP_TYPE_NORMAL
         }
 
         googleMap?.run {
             animateCamera(CameraUpdateFactory.newLatLngZoom(origin, 15F))
+            uiSettings.isZoomControlsEnabled = true
+            uiSettings.isMyLocationButtonEnabled = true
+            uiSettings.isScrollGesturesEnabled = true
+
+            listParadas.forEach {
+                addMarker(
+                    MarkerOptions()
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_parada))
+                        .position(LatLng(it.py, it.px))
+                        .title(it.np)
+                        .snippet("${it.cp} - ${it.ed}")
+                )
+            }
+
+            listPosVeiculos.forEach {
+                addMarker(
+                    MarkerOptions()
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_bus))
+                        .position(LatLng(it.py, it.px))
+                        .title(it.p)
+                        .snippet((if (it.a) "Veiculo acessível para pessoas com deficiencia" else "Veiculo não acessível para pessoas com deficiencia"))
+                )
+            }
         }
 
         Log.d("HSV", listParadas.size.toString())
